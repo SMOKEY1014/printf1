197c53b5d8ae7a006f9fda32f9600d446007221d
SMOKEY1014 <smokeydhlamini@gmail.com>
2023-07-24 12:36:20 +0200

    fix

--- printf.c
+++ printf.c
@@ -1,81 +1,78 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdarg.h>
 #include <unistd.h>
-#include <stdlib.h>
 
-
-
-
-int _printf(const char *format, ...)
-{
+int _printf(const char *format, ...) {
 	va_list args;
 	va_start(args, format);
 
-	int count_char = 0;
+	int char_count = 0;
+	char ch;
+	char *str;
+	int len;
 
-	while (*format != '\0')
-	{
-		if (*format == '%')
-		{
+	while (*format != '\0') {
+		if (*format == '%') {
 			format++; // Move past '%'
-			switch (*format)
-			{
+			switch (*format) {
 				case 'c':
-					{
-						  char ch = va_arg(args, int);
-						  count_char += write_ch_stdout(ch);
-						  break;
+					ch = va_arg(args, int);
+					if (write(1, &ch, 1) == -1) {
+						va_end(args);
+						return -1; // Error occurred during write
 					}
+					char_count++;
+					break;
 				case 's':
-					{
-						char *str = va_arg(args, char *);
-						if (str != NULL)
-						{
-							int len = 0;
-							while (str[len] != '\0')
-								len++;
-							if (write(1, str, len) == -1)
-							{
-								va_end(args);
-								return -1; // Error occurred during write
-							}
-							count_char += len;
+					str = va_arg(args, char *);
+					if (str != NULL) {
+						len = 0;
+						while (str[len] != '\0')
+							len++;
+						if (write(1, str, len) == -1) {
+							va_end(args);
+							return -1; // Error occurred during write
 						}
-						break;
+						char_count += len;
 					}
+					break;
 				case '%':
-					{
-						count_char += write_ch_stdout('%');
-						break;
+					ch = '%';
+					if (write(1, &ch, 1) == -1) {
+						va_end(args);
+						return -1; // Error occurred during write
 					}
+					char_count++;
+					break;
 				default:
-					{
-						// Ignore invalid specifier or just print the character
-						count_char += write_ch_stdout(*format);
-						break;
+					// Ignore invalid specifier or just print the character
+					ch = *format;
+					if (write(1, &ch, 1) == -1) {
+						va_end(args);
+						return -1; // Error occurred during write
 					}
+					char_count++;
+					break;
 			}
-		}
-		else
-		{
-			char ch = *format;
-			if (write(1, &ch, 1) == -1)
-			{
+		} else {
+			ch = *format;
+			if (write(1, &ch, 1) == -1) {
 				va_end(args);
 				return -1; // Error occurred during write
 			}
-			count_char++;
+			char_count++;
 		}
 		format++;
 	}
 
 	va_end(args);
-	return count_char;
+	return char_count;
 }
 
-static int write_ch_stdout(char ch)
-{
-	if (write(1, &ch, 1) == -1)
-		return -1; // Error occurred during write
-	return 1;
+int main() {
+	int chars_printed = _printf("Hello, %s! The character is: %c%%\n", "world", 'A');
+	printf("Number of characters printed: %d\n", chars_printed);
+	return 0;
 }
+
