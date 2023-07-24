9809208e829a23ed7573657f0f1b1637eb8ffb91
SMOKEY1014 <smokeydhlamini@gmail.com>
2023-07-24 15:03:03 +0200

    I tired

--- New/_printf.c
+++ New/_printf.c
@@ -10,8 +10,7 @@ int _printf(const char * const format, ...)
 		{"%s", printf_string}, {"%c", printf_char},
 		{"%%", printf_37},
 		{"%i", printf_int}, {"%d", printf_dec}, {"%r", printf_srev},
-		{"%R", printf_rot13}, {"%b", printf_bin}, {"%u", printf_unsigned},
-		{"%o", printf_oct}, {"%x", printf_hexa}, {"%X", printf_HEX},
+		{"%R", printf_rot13}, {"%b", printf_bin}, {"%u", printf_unsigned},		 {"%x", printf_hex}, {"%X", printf_HEX},
 		{"%S", printf_exclusive_string}, {"%p", printf_pointer}
 	};
 
